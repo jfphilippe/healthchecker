3d1eada249fdc048ed11064c59b11b97dd18b425
Jean-Francois PHILIPPE <jfphilippe.gdf@gmail.com>
2018-01-14 15:29:19 +0100

    add a param

--- main.go
+++ main.go
@@ -2,19 +2,27 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"net/http"
 	"os"
 )
 
 func main() {
 	port := flag.String("port", "80", "port on localhost to check")
-  url := flag.String("url", "/health", "local url to check (start with /)")
+	endpoint := flag.String("endpoint", "/health", "local url to check (start with /)")
 	flag.Parse()
 
-	resp, err := http.Get("http://127.0.0.1:" + *port + *url)    // Note pointer dereference using *
-	
+	url := "http://127.0.0.1:" + *port + *endpoint
+	fmt.Printf("URL : %s\n", url)
+	resp, err := http.Get(url) // Note pointer dereference using *
+
 	// If there is an error or non-200 status, exit with 1 signaling unsuccessful check.
 	if err != nil || resp.StatusCode != 200 {
+		if nil != err {
+			fmt.Printf("FAIL Error : %s\n", err)
+		} else {
+			fmt.Printf("FAIL StatusCode : %d\n", resp.StatusCode)
+		}
 		os.Exit(1)
 	}
 	os.Exit(0)
