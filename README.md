be6ddc17aa3cee0ccef57b59a23cb5a388767181
Jean-Francois PHILIPPE <jfphilippe.gdf@gmail.com>
2018-04-02 15:55:51 +0200

    Update code to handle 2xx status code

--- README.md
+++ README.md
@@ -1,2 +1,53 @@
 # healthchecker
-Go Main program for Docker Healtcheck
+
+Go Main program for Docker Healtcheck.
+
+## Description
+
+This program will call an URL, and return 0 if call succeded (2xx status code), or 1 otherwise.
+
+## Options
+
+- debug : will print to stdout debug infos
+- endpoint : force a specific local end point. Must start with `/`. Default to `/heath`
+- ip : IP address to call. default to 127.0.0.1
+- port : port to call, default to 80
+- timeout : timeout for the http call, default to 20s.
+
+Exemples :
+
+```shell
+ healthcheck -debug -ip=192.168.0.1 -port=5010 -endpoint=/
+ healthcheck -timeout=50s
+```
+
+## Dockerfile
+
+May be used in DockerFile as an HEALTH checker.
+
+```Dockerfile
+# Create a minimal whoami docker image
+# with multi-stage build
+# require a "recent" docker version.
+FROM golang:1.9.2-stretch as compiler
+
+# Copy file from current dir
+WORKDIR /go/src/github.com/jfphilippe/whoami
+COPY *.go /go/src/github.com/jfphilippe/whoami/
+# Compile
+RUN CGO_ENABLED=0 go install --tags netgo -a -ldflags '-s -w' .
+
+# add healthchecker
+RUN CGO_ENABLED=0 go get --tags netgo -a -ldflags '-s -w' github.com/jfphilippe/healthchecker
+
+FROM scratch
+COPY --from=compiler /go/bin/whoami /whoami
+COPY --from=compiler /go/bin/healthchecker /healthchecker
+EXPOSE 3000
+
+# Healthcheck in exec mode, because scratch has no shell
+HEALTHCHECK --interval=5s --timeout=3s --retries=3 \
+        CMD [ "/healthchecker", "-port=3000" ]
+
+ENTRYPOINT ["/whoami"]
+```
--- main.go
+++ main.go
@@ -1,25 +1,55 @@
+/*
+Program to test and HTTP end point
+Copyright (c) 2018 Jean-François PHILIPPE
+
+Try to call some local URL (127.0.0.1) , return 0 if the URL returns a 2xx code, 1 otherwise
+*/
 package main
 
 import (
 	"flag"
+	"fmt"
 	"net/http"
 	"os"
+	"strconv"
+	"time"
 )
 
 func main() {
 	debug := flag.Bool("debug", false, "print debug infos")
-	port := flag.String("port", "80", "port on localhost to check")
+	port := flag.Uint("port", 80, "HTTP port of URL check")
 	endpoint := flag.String("endpoint", "/health", "local url to check (start with /)")
+	timeout := flag.Duration("timeout", time.Second*10, "HTTP timeout in seconds")
+	ip := flag.String("ip", "127.0.0.1", "IP Address of end point")
+
+	flag.Usage = func() {
+		fmt.Fprintf(os.Stderr, "Health checker\n")
+		fmt.Fprintf(os.Stderr, "    version: %s\n", "0.1")
+		fmt.Fprintf(os.Stderr, "    copyright: %s\n", "jeff")
+		fmt.Fprintf(os.Stderr, "Usage of %s:\n", os.Args[0])
+		flag.PrintDefaults()
+	}
+
 	flag.Parse()
 
-	url := "http://127.0.0.1:" + *port + *endpoint
+	// build the URL to call
+	url := "http://" + *ip
+	if 80 != *port {
+		url = url + ":" + strconv.Itoa(int(*port))
+	}
+	url = url + *endpoint
 	if *debug {
 		print("URL : " + url + "\n")
 	}
-	resp, err := http.Get(url) // Note pointer dereference using *
+	// Create a Client & Specify a timeout
+	var netClient = &http.Client{
+		Timeout: *timeout,
+	}
 
-	// If there is an error or non-200 status, exit with 1 signaling unsuccessful check.
-	if nil != err || 200 != resp.StatusCode {
+	resp, err := netClient.Get(url)
+
+	// If there is an error or non-2xx status, exit with 1 signaling unsuccessful check.
+	if nil != err || (200 > resp.StatusCode || 300 <= resp.StatusCode) {
 		if *debug {
 			if nil != err {
 				print("FAIL Error : ")
